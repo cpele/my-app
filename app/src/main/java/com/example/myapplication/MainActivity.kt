338fdfa8b83051682b486fe4d924c54de435ed2a
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2024-07-03 16:36:08 +0200

    Change 1st screen message

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Fubo",
+                        name = "Molotov",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
