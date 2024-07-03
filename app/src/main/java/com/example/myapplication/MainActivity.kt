407b3b33496c0c0699ada7f10a684b1367672246
Christophe Pelé <97438970+cpele@users.noreply.github.com>
2024-07-03 16:33:15 +0200

    Add 2nd screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.myapplication
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,22 +11,36 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
+enum class Screen { First, Second }
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Fubo",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    var screen by remember {
+                        mutableStateOf(Screen.First)
+                    }
+                    when (screen) {
+                        Screen.First ->
+                            Greeting(
+                                name = "Fubo",
+                                modifier = Modifier.padding(innerPadding)
+                            ) { screen = Screen.Second }
+
+                        Screen.Second ->
+                            GoodJob(name = "Fubo")
+                    }
                 }
             }
         }
@@ -35,29 +48,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier, doIt: () -> Unit) {
     Column {
         Text(
             text = "Hello $name!",
             modifier = modifier
         )
         val context = LocalContext.current
-        Button(onClick = {
-            Toast.makeText(
-                context,
-                "To do: button action",
-                Toast.LENGTH_SHORT
-            ).show()
-        }) {
+        Button(onClick = { doIt() }) {
             Text("Go!")
         }
     }
+}
+
+@Composable
+fun GoodJob(name: String) {
+    Text("Good job $name, carry on!")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Greeting("Android") {}
     }
 }
